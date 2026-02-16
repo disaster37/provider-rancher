f3f0122242139fede5d4b38cba89093a76640e51
disaster37 <linuxworkgroup@hotmail.com>
2026-02-16 10:51:06 +0000

    From scratch

--- apis/v1beta1/doc.go
+++
@@ -1,9 +0,0 @@
-/*
-Copyright 2022 Upbound Inc.
-*/
-
-// Package v1beta1 contains the core resources of the rancher upjet provider.
-// +kubebuilder:object:generate=true
-// +groupName=rancher.contrib.crossplane.io
-// +versionName=v1beta1
-package v1beta1
